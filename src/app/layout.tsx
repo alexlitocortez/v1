924ffd22f2d5164efaea31bb8012d260c2028e60
Alex <acim650@gmail.com>
2024-04-01 23:13:12 -0700

    add auth route, navbar, and landing page sections

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,10 @@
 import "~/styles/globals.css";
+import { cn } from "../../src/app/lib/utils"
 
 import { Inter } from "next/font/google";
 
 import { TRPCReactProvider } from "~/trpc/react";
+import Navbar from "./_components/Navbar";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -21,9 +23,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={`font-sans ${inter.variable}`}>
-        <TRPCReactProvider>{children}</TRPCReactProvider>
+    <html lang="en" className="light">
+      <body className={cn('min-h-screen font-sans antialiased grainy', inter.className)}>
+        <TRPCReactProvider><Navbar />{children}</TRPCReactProvider>
       </body>
     </html>
   );
