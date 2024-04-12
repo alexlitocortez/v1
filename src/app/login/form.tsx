c510523229e6cc1d99f682786ceb857343ee7f57
Alex <acim650@gmail.com>
2024-04-11 22:38:08 -0700

    added login form

--- src/app/login/form.tsx
+++ src/app/login/form.tsx
@@ -4,47 +4,19 @@ import { useState } from "react"
 import { Button } from "~/components/ui/button"
 import { Input } from "~/components/ui/input"
 import { Label } from "~/components/ui/label"
-import { Mutation, Query, useMutation, useQuery } from "@tanstack/react-query"
+import { Mutation, useMutation } from "@tanstack/react-query"
 import { api } from "~/trpc/react"
-import { useRouter } from "next/router"
 
 
-export const RegisterForm = () => {
+export const LoginForm = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     // const userQuery = api.register.registerMessage.useMutation(Mutation.)
-    // const { query } = useRouter()
-    const hello = api.example.hello.useQuery({ text: "from tRPC" })
 
     const onSubmit = async (e: React.FormEvent) => {
         e.preventDefault()
-
-        try {
-            const res = await fetch('/api/register', {
-                method: 'POST',
-                body: JSON.stringify({
-                    email,
-                    password
-                }),
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            })
-            if (res.ok) {
-                // redirect
-            }
-        } catch (error) {
-            console.error(error)
-        }
-
-        console.log('email', email)
-        console.log('password', password)
+        console.log("Login!")
     }
-
-    const handleRegistration = () => {
-        return hello
-    }
-
 
 
     return (
@@ -58,7 +30,7 @@ export const RegisterForm = () => {
                 <Input value={password} onChange={(e) => setPassword(e.target.value)} id="password" type="password" />
             </div>
             <div className="w-full">
-                <Button className="w-full" onClick={handleRegistration}>Register</Button>
+                <Button className="w-full">Login</Button>
             </div>
         </form>
     )
