1eed5fcb8886d425b90c520adf0b001d2d45893b
Alex <acim650@gmail.com>
2024-04-14 17:15:17 -0700

    pushed schema to SQLbench

--- src/app/register/form.tsx
+++ src/app/register/form.tsx
@@ -10,55 +10,53 @@ import { useRouter } from "next/router"
 
 
 export const RegisterForm = () => {
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    // const userQuery = api.register.registerMessage.useMutation(Mutation.)
+    const [formData, setFormData] = useState({ email: '', password: '' })
+    // const registered = api.register.create.useMutation()
     // const { query } = useRouter()
-    const hello = api.example.hello.useQuery({ text: "from tRPC" })
+
 
     const onSubmit = async (e: React.FormEvent) => {
         e.preventDefault()
 
         try {
-            const res = await fetch('/api/register', {
-                method: 'POST',
-                body: JSON.stringify({
-                    email,
-                    password
-                }),
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            })
-            if (res.ok) {
-                // redirect
-            }
+            // const res = await fetch('/api/trpc', {
+            //     method: 'POST',
+            //     body: JSON.stringify({
+            //         email,
+            //         password
+            //     }),
+            //     headers: {
+            //         'Content-Type': 'application/json'
+            //     }
+            // })
+            // if (res.ok) {
+            // }
         } catch (error) {
             console.error(error)
         }
 
-        console.log('email', email)
-        console.log('password', password)
+        console.log('email', formData.email)
+        console.log('password', formData.password)
     }
 
-    const handleRegistration = () => {
-        return hello
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value })
     }
 
 
 
     return (
-        <form onSubmit={onSubmit} className="space-y-12 w-[400px]">
+        <form className="space-y-12 w-[400px]">
             <div className="grid w-full max-w-sm items-center gap-1.5">
                 <Label htmlFor="email">Email</Label>
-                <Input value={email} onChange={(e) => setEmail(e.target.value)} id="email" type="email" />
+                <Input value={formData.email} onChange={handleChange} id="email" type="email" />
             </div>
             <div>
                 <Label htmlFor="password">Password</Label>
-                <Input value={password} onChange={(e) => setPassword(e.target.value)} id="password" type="password" />
+                <Input value={formData.password} onChange={handleChange} id="password" type="password" />
             </div>
             <div className="w-full">
-                <Button className="w-full" onClick={handleRegistration}>Register</Button>
+                <Button className="w-full">Register</Button>
             </div>
         </form>
     )
