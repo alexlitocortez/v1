8c47eae38032f3ae4bee903655a415d44c6dfef3
Alex <acim650@gmail.com>
2024-05-12 19:14:13 -0700

    figured out validating data in backend

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,32 +1,46 @@
 import { type NextApiRequest, type NextApiResponse } from 'next';
-import { NextResponse } from 'next/server'
+import { NextResponse, NextRequest } from 'next/server'
 import * as cheerio from 'cheerio';
 import axios from 'axios';
-import puppeteer from 'puppeteer';
 import { error } from 'console';
 
-// export async function GET(request: Request) {
-//     const responseString = 'Hello world!'
-//     return NextResponse.json({ message: responseString })
+
+export async function POST(req: Request) {
+    const data = await req.json() as string
+    console.log("data received", data)
+
+    const response = await axios.get(data)
+
+    const html = response.data as string
+
+    const $ = cheerio.load(html)
+
+    const downloads = $('.hero__subtitle p').text()
+    console.log("downloads", downloads)
+
+    return NextResponse.json({ message: 'get it!' })
+}
+
+// try {
+
+//     // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+//     const response = await axios.get(data);
+
+//     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+//     const html = response.data;
+
+//     // Load HTML content into Cheerio
+//     // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+//     const $ = cheerio.load(html);
+
+//     // Extract data from the webpage using Cheerio selectors
+//     const downloads = $('.markdown p').text();
+
+//     // Return the extracted data
+//     return NextResponse.json({ downloads: downloads })
+// } catch (error) {
+//     console.error('Error fetching data:', error)
+//     res.status(500).json({ error: 'An error occurred while fetching data' })
 // }
 
-export async function GET(req: NextApiRequest, res: NextApiResponse) {
-    try {
-        const response = await axios.get('https://cheerio.js.org/blog');
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-        const html = response.data;
 
-        // Load HTML content into Cheerio
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-        const $ = cheerio.load(html);
-
-        // Extract data from the webpage using Cheerio selectors
-        const downloads = $('.markdown p').text();
-
-        // Return the extracted data
-        return NextResponse.json({ downloads: downloads })
-    } catch (error) {
-        console.error('Error fetching data:', error)
-        res.status(500).json({ error: 'An error occurred while fetching data' })
-    }
-}
